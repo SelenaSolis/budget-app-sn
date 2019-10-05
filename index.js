fdfe772b7ab9532e8c31c2bfbba5e5464dd847fa
Selena Solis <lenansolis@gmail.com>
2019-10-04 20:33:11 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+let userRoute = require("./server/routes/userRoute")
+const bodyParser = require("body-parser");
+let mongoose = require("mongoose");
+require("dotenv").config();
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use('/users', userRoute);
+
+mongoose.connect(process.env.mongodburi, {useNewUrlParser: true});
+
+
+const thePort = 3001
+app.listen(thePort, (err) => {
+ if (err) {
+   return console.log("Error", err);
+ }
+ console.log("Web server is now listening for messages on port",thePort);
+});
